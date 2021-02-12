52317beded7acc53370ec0f9454d8be9bdd7aa82
Sean McCarthy <smccarthy0426@gmail.com>
2021-02-12 14:18:12 -0800

    Testing user status change

--- app.js
+++ app.js
@@ -50,10 +50,7 @@ app.event('user_change', async ({ event, client, context }) => {
       if(status.includes("Kitchen")){
           const result = await client.chat.postMessage({
           channel: user.id,
-          text: {
-            "type" : "mrkdwn",
-            "text" : "<http://g.co/meet/kitchenslack1|This message *is* a link>"
-          }
+          text: "Would you like to join kitchen chat? <http://g.co/meet/kitchenslack1|Join here!"
         });
       }
   }
