ed4fd0cb03d8d224d5b5c8ae9573186aab1c97dc
Tushar Patil <tpatil@salesforce.com>
2021-02-12 12:53:28 -0800

    tushar ttest commit

--- app.js
+++ app.js
@@ -106,5 +106,5 @@ app.event('app_home_opened', async ({ event, client, context }) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('⚡️ Bolt app is running!!');
 })();
