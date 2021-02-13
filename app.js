b137db72c720b4fc10bbd35f6a8030062fea4a87
Tushar Patil <tpatil@salesforce.com>
2021-02-12 22:05:14 -0800

    pushing

--- app.js
+++ app.js
@@ -62,12 +62,12 @@ app.event('user_change', async ({ event, client, context }) => {
             console.log('error usersWithKitchenStatus:', err.stack);
           } else {
             console.log('Result usersWithKitchenStatus:',res);
-            /*const result = client.chat.postMessage({
-              channel: user.id,
-              text: "Would you like to join kitchen chat? <http://g.co/meet/kitchenslack1|Join here!>"
-            });*/
           }
         });
+        const result = await client.chat.postMessage({
+              channel: user.id,
+              text: "Would you like to join kitchen chat? <http://g.co/meet/kitchenslack1|Join here!>"
+            });
       } else {
         users.upsertUserWithStatus(user.id, false);
       }
