d0b3a4c7a5463f21bce4f3c03c7f500ebcff0cc5
Michael Dowd <mdowd@salesforce.com>
2021-02-12 16:04:08 -0800

    fix missed

--- users.js
+++ users.js
@@ -3,7 +3,7 @@ var db = require('./db');
 let upsert = "INSERT INTO kitchen_users(slack_id, in_kitchen) VALUES($1,$2) ON CONFLICT('slack_id') DO UPDATE SET in_kitchen=EXCLUDED.in_kitchen;";
 
 module.exports = {
-    upsertUser: (slackId, inKitchen){
+    upsertUser: (slackId, inKitchen) => {
         db.query(upsert, [slackId, inKitchen]);
     }
 }
