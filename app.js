627e0048b3de2174aa0f4510abaf1007ffd560e3
Michael Dowd <mdowd@salesforce.com>
2021-02-12 17:24:28 -0800

    change method name; update chat_id in query; add pseudo-code

--- app.js
+++ app.js
@@ -46,13 +46,23 @@ app.event('user_change', async ({ event, client, context }) => {
       var status = user.profile.status_text;
 
       if(status.includes("Kitchen")){
-        users.upsertUser(user.id, true);
+        users.upsertUserWithStatus(user.id, true);
+        users.queryUsersInRooms((err,res)=>{
+          // any chats with <5 people?
+            // select one of those chats
+            // update this user with that chat id
+            // send meet link to this user
+          // are there >=3 people waiting, not in chat?
+            // create a new chat -> with that id, create the meet name
+            // update those users with that chat id
+            // send meet link to those users
+        });
         const result = await client.chat.postMessage({
           channel: user.id,
           text: "Would you like to join kitchen chat? <http://g.co/meet/kitchenslack1|Join here!>"
         });
       } else {
-        users.upsertUser(user.id, false);
+        users.upsertUserWithStatus(user.id, false);
       }
   }
   catch(error){
--- users.js
+++ users.js
@@ -1,10 +1,10 @@
 var db = require('./db');
 
 let checkUserCount = "SELECT COUNT(slack_id) AS current_users, chat_id FROM kitchen_users WHERE in_kitchen = true GROUP BY chat_id;";
-let upsert = "INSERT INTO kitchen_users(slack_id, in_kitchen) VALUES($1,$2) ON CONFLICT(slack_id) DO UPDATE SET in_kitchen=EXCLUDED.in_kitchen;";
+let upsert = "INSERT INTO kitchen_users(slack_id, in_kitchen, chat_id) VALUES($1,$2,null) ON CONFLICT(slack_id) DO UPDATE SET in_kitchen=EXCLUDED.in_kitchen, chat_id=null;";
 
 module.exports = {
-    upsertUser: (slackId, inKitchen) => {
+    upsertUserWithStatus: (slackId, inKitchen) => {
         db.query(upsert, [slackId, inKitchen]);
     },
     queryUsersInRooms : function(callback){
