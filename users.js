073517e09d958e118ef95ab973152b2c2b557d56
Michael Dowd <mdowd@salesforce.com>
2021-02-12 16:36:59 -0800

    sql

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 var db = require('./db');
 
-let checkUserCount = "SELECT COUNT(slack_id), chat_id FROM kitchen_users GROUP BY chat_id WHERE in_kitchen = true;";
+let checkUserCount = "SELECT COUNT(slack_id) AS current_users, chat_id FROM kitchen_users WHERE in_kitchen = true GROUP BY chat_id;";
 let upsert = "INSERT INTO kitchen_users(slack_id, in_kitchen) VALUES($1,$2) ON CONFLICT(slack_id) DO UPDATE SET in_kitchen=EXCLUDED.in_kitchen;";
 
 module.exports = {
