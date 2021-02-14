7d26ff5d07c989698fd5ecf64223df83cf8ea658
Michael Dowd <mdowd@salesforce.com>
2021-02-13 17:14:53 -0800

    query stuff

--- users.js
+++ users.js
@@ -4,7 +4,7 @@ let checkUserCount = "SELECT COUNT(slack_id) AS current_users, chat_id FROM kitc
 let queryUsersWithKitchenStatus = "Select slack_id, in_kitchen, chat_id from kitchen_users where in_kitchen = true;"
 let upsert = "INSERT INTO kitchen_users(slack_id, in_kitchen, chat_id) VALUES($1,$2,null) ON CONFLICT(slack_id) DO UPDATE SET in_kitchen=EXCLUDED.in_kitchen, chat_id=null;";
 let availableUsersNotChatting = "SELECT slack_id FROM kitchen_users WHERE in_kitchen = true AND chat_id is null;";
-let updateUsers = "UPDATE kitchen_users SET in_kitchen=0 WHERE slack_id IN ($1);";
+let updateUsers = "UPDATE kitchen_users SET in_kitchen='false' WHERE slack_id IN ($1);";
 
 module.exports = {
     upsertUserWithStatus: (slackId, inKitchen, callback) => {
